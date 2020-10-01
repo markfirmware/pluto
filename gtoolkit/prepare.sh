c25d60cb0bf6c07f6ab8df9dacc6a4ec026107a1
markfirmware <markfirmware@users.noreply.github.com>
2020-10-01 00:43:34 -0400

    gtoolkit

---
+++ gtoolkit/prepare.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd gtoolkit
+wget dl.feenk.com/gt/GlamorousToolkitLinux64-v0.8.186.zip
+unzip GlamorousToolkitLinux64-v0.8.186.zip
---
+++ gtoolkit/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd gtoolkit/GlamorousToolkitLinux64-v0.8.186
+./glamoroustoolkit GlamorousToolkit.image --no-quit --interactive
